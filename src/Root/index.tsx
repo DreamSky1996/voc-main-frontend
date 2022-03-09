fd284b22b3201efa51adecbeeac1852cea8fc30d
DreamSky1996 <webhipe@gmail.com>
2022-03-10 00:50:55 +0900

    add index.tsx on root folder

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -1,106 +1,24 @@
-import { useEffect, useState, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
-import { IReduxState } from "../store/slices/state.interface";
-import { loadAppDetails } from "../store/slices/app-slice";
-import { loadAccountDetails } from "../store/slices/account-slice";
-import { useAddress, useWeb3Context } from "../hooks";
+import React, { useEffect, useState } from "react";
+import App from "./app";
+import { HashRouter } from "react-router-dom";
 import { loadTokenPrices } from "../helpers";
-import ViewBase from "../components/ViewBase";
 import Loading from "../components/Loader";
-import { Dashboard, VOC, Shares, NotFound } from "../views";
 import "./style.scss";
 
 
 
 function Root() {
-    const dispatch = useDispatch();
-    const { connect, provider, hasCachedProvider, chainID, connected } = useWeb3Context();
-    const address = useAddress();
-    const [walletChecked, setWalletChecked] = useState(false);
     const [loading, setLoading] = useState(true);
-
-   
-
-    
-
-    const isAppLoading = useSelector<IReduxState, boolean>(state => state.app.loading);
-    
-    async function loadDetails(whichDetails: string) {
-        let loadProvider = provider;
-
-        if (whichDetails === "app") {
-            loadApp(loadProvider);
-        }
-
-        if (whichDetails === "account" && address && connected) {
-            loadApp(loadProvider);
-            loadAccount(loadProvider);
-        }
-    }
-
-
-    const loadApp = useCallback(
-        loadProvider => {
-            dispatch(loadAppDetails({ networkID: chainID, provider: loadProvider }));
-        },
-        [connected],
-    );
-
-    const loadAccount = useCallback(
-        loadProvider => {
-            dispatch(loadAccountDetails({ address, networkID: chainID, provider: loadProvider }));
-        },
-        [connected],
-    );
 
     useEffect(() => {
         loadTokenPrices().then(() => setLoading(false));
-        if (hasCachedProvider()) {
-            connect().then(() => {
-                setWalletChecked(true);
-            });
-        } else {
-            setWalletChecked(true);
-        }
     }, []);
-
-    useEffect(() => {
-        if (walletChecked) {
-            loadDetails("app");
-            loadDetails("account");
-        }
-    }, [walletChecked]);
-
-    useEffect(() => {
-        if (connected) {
-            loadDetails("app");
-            loadDetails("account");
-        }
-    }, [connected]);
 
     if (loading) return <Loading />;
 
     return (
         <HashRouter>
-            <ViewBase>
-            <Switch>
-                <Route exact path="/dashboard">
-                    <Dashboard />
-                    <VOC />
-                </Route>
-
-                <Route exact path="/">
-                    <Redirect to="/dashboard" />
-                </Route>
-
-                <Route path="/shares">
-                    <Shares />
-                </Route>
-
-                <Route component={NotFound} />
-            </Switch>
-            </ViewBase>
+            <App/>
         </HashRouter>
     );
 }
--- src/helpers/token-price.ts
+++ src/helpers/token-price.ts
@@ -6,13 +6,11 @@ export const loadTokenPrices = async () => {
     const url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
     const { data } = await axios.get(url);
 
-    cache["ETH"] = data["ethereum"]["usd"];
-    console.log(data["ethereum"]["usd"]);
+    cache["ETH"] = data["ethereum"].usd;
     // cache["ETH"] = 2742.52;
     
 };
 
 export const getTokenPrice = (symbol: string): number => {
-        console.log(Number(cache[symbol]));
         return Number(cache[symbol]);
 };
