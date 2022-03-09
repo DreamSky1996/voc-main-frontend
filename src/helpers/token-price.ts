0473ae0d3ad27c44dc5d935e0a7554a0ec35dffa
DreamSky1996 <webhipe@gmail.com>
2022-03-10 00:21:20 +0900

    add coingecko api

--- src/helpers/token-price.ts
+++ src/helpers/token-price.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const cache: { [key: string]: number } = {};
 
 export const loadTokenPrices = async () => {
-    // const url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
-    // const { data } = await axios.get(url);
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
+    const { data } = await axios.get(url);
 
-    // cache["ETH"] = data["ethereum"].usd;2742.52
-    cache["ETH"] = 2742.52;
+    cache["ETH"] = data["ethereum"].usd;
+    // cache["ETH"] = 2742.52;
     
 };
 
